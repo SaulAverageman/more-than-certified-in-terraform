fa283cce414094ff7d16a2f9e51cb4191b56f464
bubaiTLE <bubai.official9119@gmail.com>
2022-08-27 22:34:18 +0530

    Sat 27 Aug 22:34:18 IST 2022

--- commit-script.sh
+++ commit-script.sh
@@ -3,7 +3,7 @@ ssh-agent bash -c "ssh-add ../id_rsa; git pull"
 git add .
 git commit -m "`date`"
 var=`hostname`
-if [ ${var:0:1} = 'n' ]
+if [ ${var:0:1} != 'n' ]
   then
     ssh-agent bash -c "ssh-add ../id_rsa; git push -u origin main"
   else
